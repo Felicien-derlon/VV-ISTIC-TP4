c1814b3fadfb6f3234057e22743c168f7f4f6f9c
« <>
2022-03-17 11:26:33 +0100

    exercice RomanNumeral

--- code/roman-numerals/src/test/java/fr/istic/vv/RomanNumeralTest.java
+++ code/roman-numerals/src/test/java/fr/istic/vv/RomanNumeralTest.java
@@ -1,9 +1,33 @@
 package fr.istic.vv;
 import net.jqwik.api.*;
+import static org.assertj.core.api.Assertions.*;
 
 public class RomanNumeralTest {
+
     @Property
-    boolean absoluteValueOfAllNumbersIsPositive(@ForAll int anInteger) {
-        return Math.abs(anInteger) >= 0;
+    void intergerToRoman(@ForAll("integers") int integer) {
+        String roman = RomanNumeraUtils.toRomanNumeral(integer);
+        if(RomanNumeraUtils.isValidRomanNumeral(roman)) {
+            assertThat(RomanNumeraUtils.parseRomanNumeral(roman)).isEqualTo(integer);
+        }
+    }
+
+    @Property
+    void romanToInteger(@ForAll("romans") String roman) {
+        if(RomanNumeraUtils.isValidRomanNumeral(roman)) {
+            int integer = RomanNumeraUtils.parseRomanNumeral(roman);
+            String intToRoman = RomanNumeraUtils.toRomanNumeral(integer);
+            assertThat(intToRoman).isEqualTo(roman);
+        }
+    }
+
+    @Provide
+    Arbitrary<Integer> integers() {
+        return Arbitraries.integers().between(1, 3999);
+    }
+
+    @Provide
+    Arbitrary<String> romans() {
+        return Arbitraries.strings().withChars(new char[] {'M','D','C','L','X','V','I'}).ofMaxLength(9);
     }
 }
